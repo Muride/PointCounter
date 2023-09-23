0a3710cc81aa1af4e53895ee993c4bc89f9af1a8
Muride <45217103+Muride@users.noreply.github.com>
2023-09-23 21:08:55 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,311 @@
+// JavaScript Document
+var amount1 = 0;
+var amount2 = 0;
+var amount3 = 0;
+var amount4 = 0;
+var amount5 = 0;
+var amount6 = 0;
+var amount7 = 0;
+var amount8 = 0;
+var amount9 = 0;
+var amount10 = 0;
+var amount11 = 0;
+var amount12 = 0;
+var amount13 = 0;
+var amount14 = 0;
+var amount15 = 0;
+var amount16 = 0;
+var amount17 = 0;
+var amount18 = 0;
+var amount19 = 0;
+var amount20 = 0;
+var amount21 = 0;
+var amount22 = 0;
+var amount23 = 0;
+var amount24 = 0;
+var amount25 = 0;
+var amount26 = 0;
+var amount27 = 0;
+var amount28 = 0;
+var amount29 = 0;
+var amount30 = 0;
+
+$(function(){
+	$("#ad2btn1").click(function(){
+		amount1 += 2;
+		$("#counter1").text(amount1);
+	});
+	$("#ad1btn1").click(function(){
+		amount1 ++;
+		$("#counter1").text(amount1);
+	});
+	
+	$("#ad2btn2").click(function(){
+		amount2 += 2;
+		$("#counter2").text(amount2);
+	});
+	$("#ad1btn2").click(function(){
+		amount2 ++;
+		$("#counter2").text(amount2);
+	});
+	
+	$("#ad2btn3").click(function(){
+		amount3 += 2;
+		$("#counter3").text(amount3);
+	});
+	$("#ad1btn3").click(function(){
+		amount3 ++;
+		$("#counter3").text(amount3);
+	});
+	
+	$("#ad2btn4").click(function(){
+		amount4 += 2;
+		$("#counter4").text(amount4);
+	});
+	$("#ad1btn4").click(function(){
+		amount4 ++;
+		$("#counter4").text(amount4);
+	});
+	
+	$("#ad2btn5").click(function(){
+		amount5 += 2;
+		$("#counter5").text(amount5);
+	});
+	$("#ad1btn5").click(function(){
+		amount5 ++;
+		$("#counter5").text(amount5);
+	});
+	
+	$("#ad2btn6").click(function(){
+		amount6 += 2;
+		$("#counter6").text(amount6);
+	});
+	$("#ad1btn6").click(function(){
+		amount6 ++;
+		$("#counter6").text(amount6);
+	});
+	
+	$("#ad2btn7").click(function(){
+		amount7 += 2;
+		$("#counter7").text(amount7);
+	});
+	$("#ad1btn7").click(function(){
+		amount7 ++;
+		$("#counter7").text(amount7);
+	});
+	
+	$("#ad2btn8").click(function(){
+		amount8 += 2;
+		$("#counter8").text(amount8);
+	});
+	$("#ad1btn8").click(function(){
+		amount8 ++;
+		$("#counter8").text(amount8);
+	});
+	
+	$("#ad2btn9").click(function(){
+		amount9 += 2;
+		$("#counter9").text(amount9);
+	});
+	$("#ad1btn9").click(function(){
+		amount9 ++;
+		$("#counter9").text(amount9);
+	});
+	
+	$("#ad2btn10").click(function(){
+		amount10 += 2;
+		$("#counter10").text(amount10);
+	});
+	$("#ad1btn10").click(function(){
+		amount10 ++;
+		$("#counter10").text(amount10);
+	});
+	
+	$("#ad2btn11").click(function(){
+		amount11 += 2;
+		$("#counter11").text(amount11);
+	});
+	$("#ad1btn11").click(function(){
+		amount11 ++;
+		$("#counter11").text(amount11);
+	});
+	
+	$("#ad2btn12").click(function(){
+		amount12 += 2;
+		$("#counter12").text(amount12);
+	});
+	$("#ad1btn12").click(function(){
+		amount12 ++;
+		$("#counter12").text(amount12);
+	});
+	
+	$("#ad2btn13").click(function(){
+		amount13 += 2;
+		$("#counter13").text(amount13);
+	});
+	$("#ad1btn13").click(function(){
+		amount13 ++;
+		$("#counter13").text(amount13);
+	});
+	
+	$("#ad2btn14").click(function(){
+		amount14 += 2;
+		$("#counter14").text(amount14);
+	});
+	$("#ad1btn14").click(function(){
+		amount14 ++;
+		$("#counter14").text(amount14);
+	});
+	
+	$("#ad2btn15").click(function(){
+		amount15 += 2;
+		$("#counter15").text(amount15);
+	});
+	$("#ad1btn15").click(function(){
+		amount15 ++;
+		$("#counter15").text(amount15);
+	});
+	
+	$("#ad2btn16").click(function(){
+		amount16 += 2;
+		$("#counter16").text(amount16);
+	});
+	$("#ad1btn16").click(function(){
+		amount16 ++;
+		$("#counter16").text(amount16);
+	});
+	
+	$("#ad2btn17").click(function(){
+		amount17 += 2;
+		$("#counter17").text(amount17);
+	});
+	$("#ad1btn17").click(function(){
+		amount17 ++;
+		$("#counter17").text(amount17);
+	});
+	
+	$("#ad2btn18").click(function(){
+		amount18 += 2;
+		$("#counter18").text(amount18);
+	});
+	$("#ad1btn18").click(function(){
+		amount18 ++;
+		$("#counter18").text(amount18);
+	});
+	
+	$("#ad2btn19").click(function(){
+		amount19 += 2;
+		$("#counter19").text(amount19);
+	});
+	$("#ad1btn19").click(function(){
+		amount19 ++;
+		$("#counter19").text(amount19);
+	});
+	
+	$("#ad2btn20").click(function(){
+		amount20 += 2;
+		$("#counter20").text(amount20);
+	});
+	$("#ad1btn20").click(function(){
+		amount20 ++;
+		$("#counter20").text(amount20);
+	});
+	
+	$("#ad2btn21").click(function(){
+		amount21 += 2;
+		$("#counter21").text(amount21);
+	});
+	$("#ad1btn21").click(function(){
+		amount21 ++;
+		$("#counter21").text(amount21);
+	});
+	
+	$("#ad2btn22").click(function(){
+		amount22 += 2;
+		$("#counter22").text(amount22);
+	});
+	$("#ad1btn22").click(function(){
+		amount22 ++;
+		$("#counter22").text(amount22);
+	});
+	
+	$("#ad2btn23").click(function(){
+		amount23 += 2;
+		$("#counter23").text(amount23);
+	});
+	$("#ad1btn23").click(function(){
+		amount23 ++;
+		$("#counter23").text(amount23);
+	});
+	
+	$("#ad2btn24").click(function(){
+		amount24 += 2;
+		$("#counter24").text(amount24);
+	});
+	$("#ad1btn24").click(function(){
+		amount24 ++;
+		$("#counter24").text(amount24);
+	});
+	
+	$("#ad2btn25").click(function(){
+		amount25 += 2;
+		$("#counter25").text(amount25);
+	});
+	$("#ad1btn25").click(function(){
+		amount25 ++;
+		$("#counter25").text(amount25);
+	});
+	
+	$("#ad2btn26").click(function(){
+		amount26 += 2;
+		$("#counter26").text(amount26);
+	});
+	$("#ad1btn26").click(function(){
+		amount26 ++;
+		$("#counter26").text(amount26);
+	});
+	
+	$("#ad2btn27").click(function(){
+		amount27 += 2;
+		$("#counter27").text(amount27);
+	});
+	$("#ad1btn27").click(function(){
+		amount27 ++;
+		$("#counter27").text(amount27);
+	});
+	
+	$("#ad2btn28").click(function(){
+		amount28 += 2;
+		$("#counter28").text(amount28);
+	});
+	$("#ad1btn28").click(function(){
+		amount28 ++;
+		$("#counter28").text(amount28);
+	});
+	
+	$("#ad2btn29").click(function(){
+		amount29 += 2;
+		$("#counter29").text(amount29);
+	});
+	$("#ad1btn29").click(function(){
+		amount29 ++;
+		$("#counter29").text(amount29);
+	});
+	
+	$("#ad2btn30").click(function(){
+		amount30 += 2;
+		$("#counter30").text(amount30);
+	});
+	$("#ad1btn30").click(function(){
+		amount30 ++;
+		$("#counter30").text(amount30);
+	});
+	
+	
+});
+
+
+
+
+
+
